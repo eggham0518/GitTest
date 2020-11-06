9a08f29d4c99a6a67ae02a36a3ba815d6b9ce409
eggham0518 <eggham0518@gmail.com>
2020-11-06 17:40:25 +0900

    text color changed

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.example.gittest.databinding.ActivityMainBinding;
@@ -14,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        setTvSize();
+        setTvColor();
     }
 
-    private void setTvSize(){
-        binding.tvHelloWorld.setTextSize(20f);
+    private void setTvColor(){
+       binding.tvHelloWorld.setTextColor(Color.RED);
     }
 }
