f1f49a44d74ebb8f21d094a0063eb540f25c81c2
skill12 <goodskill1@gmail.com>
2020-11-06 09:07:22 +0000

    fix-tvsize

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.gittest;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import android.graphics.Color;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.gittest.databinding.ActivityMainBinding;
 
@@ -16,9 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setTvColor();
+        setTvSize();
     }
 
     private void setTvColor(){
        binding.tvHelloWorld.setTextColor(Color.RED);
     }
+
+    private void setTvSize(){
+        binding.tvHelloWorld.setTextSize(20f);
+    }
+}
 }
